2f36823947cec520d65bcee4f47e3fe871e64789
Emanuele De Cupis <ema.decu@gmail.com>
2020-08-21 12:12:57 +0200

    allow remote and local execution

--- install.sh
+++ install.sh
@@ -4,6 +4,14 @@
 # cd myproject/
 # ../git-hooks/install.sh
 
+# Github references
+repo=pagopa/git-hooks
+branch=master
+
+# to use local files instead of github's
+is_local=$1
+
+
 # check if is a git repo
 if [ ! -d ".git" ]; then
   echo "Please run this script inside your target git repository"
@@ -13,10 +21,10 @@ fi
 # check if git-secrets is installed
 if [ ! -x $(command -v git-secrets > /dev/null 2>&1) ]; then
     echo "The git-secrets binary is not available. Install from source or package or check your PATH."
+    echo "Visit https://github.com/awslabs/git-secrets for instructions."
     exit 1
 fi
 
-# TODO we could avoid the array declaration and traverse the dirs
 # hooks to be installed
 declare -a hooks=(
     "post-checkout"
@@ -30,17 +38,32 @@ declare -a providers=(
     "commons"
 )
 
-# get script directory
-script_directory="$(dirname $0)"
+# copy a file from current file system or from github, depending from the value of $is_local
+function copyFile {
+    source=$1
+    dest=$2
+    if [ -z "$is_local" ]; then
+        url="https://raw.githubusercontent.com/$repo/$branch/$source"
+        echo "Download from $url"
+        curl "$url" -o "$dest"
+    else 
+        # get script directory
+        script_directory="$(dirname $0)"
+        file="$script_directory/$source"
+        echo "Copying $file"
+        cp "$file" "$dest"
+    fi
+}
+
 
 # install each hook
 mkdir -p ".git/hooks"
 for hook in "${hooks[@]}"
 do
+    source="hooks/$hook"
     dest=".git/hooks/$hook"
-    file="$script_directory/hooks/$hook"
-    echo "Copying hook file: $file"
-    cp "$file" "$dest"
+    copyFile "$source" "$dest"
+    # make the hook executable
     chmod +x "$dest"
 done
 
@@ -49,14 +72,13 @@ done
 mkdir -p ".git/git-secrets-providers"
 for provider in "${providers[@]}"
 do
+    source="providers/$provider"
     dest=".git/git-secrets-providers/$provider"
-    file="$script_directory/providers/$provider"
-    echo "Copying provider file: $file"
-    cp "$file" "$dest"
+    copyFile "$source" "$dest"
     # add patterns
-    git secrets --add-provider -- grep '^[^#[:space:]]' $dest
+    git secrets --add-provider -- grep '^[^#[:space:]]' "$dest"
     echo "Added the following patterns to block local commits:"
-    grep '^[^#[:space:]]' $dest # maybe avoid if too verbose in future
+    grep '^[^#[:space:]]' "$dest" # maybe avoid if too verbose in future
 done
 
 printf "\nAll done!\n"
